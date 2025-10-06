f96de0e279b55d02f2fb37941963d65897f93f6d
Regis <regis.lopes.eso@gmail.com>
2025-10-06 17:32:56 -0300

    Add Aula 09b - example for v-for

--- 2_projeto_vue/src/components/Info.vue
+++ 2_projeto_vue/src/components/Info.vue
@@ -1,35 +1,31 @@
 <template>
   <div>
-    <p v-if="esta_trabalhando">Estou trabalho no momento.</p>
-    <p v-else>Estou em busca de novas oportunidades</p>
-    <p>
-      Utilizo as seguintes tecnologias:
-    </p>
+    <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+    <p v-else>Estou em busca de novas oportunidades.</p>
+
+    <p>Utilizo as seguintes tecnologias:</p>
     <ul>
-      <li>
-        JavaScript
-      </li>
-      <li>
-        PHP
-      </li>
-      <li>
-        Python
+      <!-- v-for example -->
+      <li v-for="(tech, index) in tecnologias" :key="index">
+        {{ tech }}
       </li>
     </ul>
+
     <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "Info",
-    data() {
-      return {
-        esta_trabalhando: true,
-        mostrar_email: false,
-        email: "matheus@email.com"
-      }
-    }
+export default {
+  name: "Info",
+  data() {
+    return {
+      esta_trabalhando: true,
+      mostrar_email: false,
+      email: "matheus@email.com",
+      // Array used by v-for
+      tecnologias: ["JavaScript", "PHP", "Python"]
+    };
   }
-
+};
 </script>
