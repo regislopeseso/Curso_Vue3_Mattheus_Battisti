5fb6ff22ac14fada8f798903638587d4b5b4dec2
Regis <regis.lopes.eso@gmail.com>
2025-10-06 16:16:00 -0300

    Add Aula 06 - Dados em componentes

--- 2_projeto_vue/src/App.vue
+++ 2_projeto_vue/src/App.vue
@@ -1,13 +1,25 @@
 <template>
-  <PrimeiroComponente />
+  <div>
+    <PrimeiroComponente />
+    <p>O dado de app é: {{ testando }}</p>
+    <LifeCycle />
+
+  </div>
 </template>
 
 <script>
-import PrimeiroComponente from "./components/PrimeiroComponente.vue"
+    import PrimeiroComponente from "./components/PrimeiroComponente.vue"
+    import LifeCycle from "./components/LifeCycle.vue"
   export default {
     name: "App",
     components: {
-      PrimeiroComponente
+      PrimeiroComponente,
+      LifeCycle
+    },
+    data() {
+      return {
+        testando: "TesTe"
+      }
     }
   }
 </script>
