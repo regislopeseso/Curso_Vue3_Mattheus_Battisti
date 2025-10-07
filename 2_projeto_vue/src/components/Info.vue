36f383fb1e6131f89cefc930c49de3d949115cf6
Regis <regis.lopes.eso@gmail.com>
2025-10-07 11:08:34 -0300

    Add Aula 13 - Renderização de listas (v-for)

--- 2_projeto_vue/src/components/Info.vue
+++ 2_projeto_vue/src/components/Info.vue
@@ -3,11 +3,19 @@
     <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
     <p v-else>Estou em busca de novas oportunidades.</p>
 
-    <p>Utilizo as seguintes tecnologias:</p>
+    <p>Utilizo as seguintes tecnologias para back-end:</p>
     <ul>
       <!-- v-for example -->
-      <li v-for="(tech, index) in tecnologies" :key="index">
-        {{ tech }}
+      <li v-for="(technology, index) in backend_technologies" v-bind:key="index">
+        {{ technology }}
+      </li>
+
+      
+    </ul>
+    <p>Utilizo as seguintes tecnologias para front-end:</p>
+    <ul>
+      <li v-for="technology in frontend_technologies" :key="technology.id">
+        {{ technology.language }}
       </li>
     </ul>
 
@@ -34,9 +42,14 @@ export default {
       esta_trabalhando: true,
       mostrar_email: false,
       email: "matheus@email.com",   
-      tecnologies: ["JavaScript", "PHP", "Python"],
       meu_link: "https://google.com",
-      textoBotao: "Mostrar e-mail"
+      textoBotao: "Mostrar e-mail",
+      backend_technologies: ["JavaScript", "PHP", "Python"],
+      frontend_technologies: [
+        {id: 1, language: "HTML"},
+        {id: 2, language: "CSS"},
+        {id: 3, language: "Vue"}
+      ],
     };
   },
   methods: {
