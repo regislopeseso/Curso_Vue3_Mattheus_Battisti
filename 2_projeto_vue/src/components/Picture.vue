31d3f9fea38d39baef1e30a4625b1b678fd376b6
Regis <regis.lopes.eso@gmail.com>
2025-10-07 14:10:23 -0300

    Add Aula 16 - Reutilizacao de componentes

--- 2_projeto_vue/src/components/Picture.vue
+++ 2_projeto_vue/src/components/Picture.vue
@@ -1,16 +1,22 @@
 <template>
     <img :src="avatar" :alt="descricao">
+    <Reutilizacao />
 </template>
 
 <script>
-export default {
-  name:"Picture",
-  data() {
-    return {
-      avatar: "/img/avatar.png",
-      descricao: "Matheus Battisti",
-    }
+  import Reutilizacao from './Reutilizacao.vue' 
+
+  export default {
+    name:"Picture",
+    data() {
+      return {
+        avatar: "/img/avatar.png",
+        descricao: "Matheus Battisti",
+      }
+    },
+    components: {
+        Reutilizacao     
+      }
   }
-}
 </script>
 
