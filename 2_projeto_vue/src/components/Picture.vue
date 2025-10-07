efbe4c985d4e54f6e4aa7e61066f64c3ec11aa5d
Regis <regis.lopes.eso@gmail.com>
2025-10-07 15:34:46 -0300

    Add Aula 18 - Emit

--- 2_projeto_vue/src/components/Picture.vue
+++ 2_projeto_vue/src/components/Picture.vue
@@ -1,22 +1,29 @@
 <template>
     <img :src="avatar" :alt="descricao">
-    <Reutilizacao />
+    <MudarImagem @mudar-imagem="trocarImagem"/>
 </template>
 
 <script>
   import Reutilizacao from './Reutilizacao.vue' 
+  import MudarImagem from './MudarImagem.vue' 
 
   export default {
     name:"Picture",
+    components: {
+        Reutilizacao,
+        MudarImagem   
+    },
     data() {
       return {
         avatar: "/img/avatar.png",
         descricao: "Matheus Battisti",
       }
     },
-    components: {
-        Reutilizacao     
+    methods: {
+      trocarImagem() {
+        this.avatar = "/img/avatar2.png"
       }
+    }
   }
 </script>
 
