48c1022a62ea8ec9d6abef0045bbef1d63680723
Regis <regis.lopes.eso@gmail.com>
2025-10-08 14:06:28 -0300

    Add Aula 29 - Resgatando pedidos do banco

--- 3_make_your_burger/src/components/Dashboard.vue
+++ 3_make_your_burger/src/components/Dashboard.vue
@@ -2,7 +2,7 @@
   <div id="burger-table">
     <div>
       <div id="burger-table-heading">
-        <div class="order-id">#:</div>
+        <div class="order-id">#</div>
         <div>Cliente</div>
         <div>Pão</div>
         <div>Carne</div>
@@ -12,15 +12,14 @@
     </div>
 
     <div id="burger-table-rows">
-      <div class="burger-table-row">
-        <div class="order-number">1</div>
-        <div>João</div>
-        <div>Pão de Trigo</div>
-        <div>Maminha</div>
+      <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
+        <div class="order-number">{{ burger.id }}</div>
+        <div>{{ burger.nome }}</div>
+        <div>{{ burger.pao }}</div>
+        <div>{{ burger.carne }}</div>
         <div>
           <ul>
-            <li>Salame</li>
-            <li>Tomate</li>
+            <li v-for="(opcional, index) in burger.opcionais" :key="index">{{ opcional }}</li>           
           </ul>
         </div>
         <div>
@@ -36,7 +35,28 @@
 
 <script>
   export default {
-    name: "Dashboard"
+    name: "Dashboard",
+    data() {
+      return {
+        burgers: null,
+        burger_id: null,
+        status: []
+      }
+    },
+    methods: {
+      async getPedidos() {
+        const req = await fetch("http://localhost:3000/burgers");
+
+        const data = await req.json();
+
+        this.burgers = data;
+
+
+      }
+    },
+    mounted() {
+      this.getPedidos();
+    }
   }
 </script>
 
